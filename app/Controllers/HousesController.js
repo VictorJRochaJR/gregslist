01e54fa23f9e91652b18d5f7c5f62d8139a21af1
VictorJRochaJR <vrocha897@gmail.com>
2021-06-01 22:15:08 -0600

    changes

--- app/Controllers/HousesController.js
+++ app/Controllers/HousesController.js
@@ -12,16 +12,37 @@ export class HousesController {
     constructor() {
         ProxyState.on('houses', drawHouses)
     }
+
+
     getHouses() {
         housesService.getHouses()
     }
     addHouse(event) {
         event.preventDefault()
         console.log(event)
-
+        let form = event.target
+        let formData = {
+            bathrooms: form.bathrooms.value,
+            bedrooms: form.bedrooms.value,
+            year: form.year.value,
+            price: form.price.value,
+            imgUrl: form.imgUrl.value,
+        }
+        if (form.houseId.value) {
+            formData.id = form.houseId.value
+            housesService.updateHouse(formData)
+        } else {
+            housesService.addHouse(formData)
+        }
+        form.reset()
+        this.toggleForm()
+    } catch(e) {
+        console.error(e.message)
     }
-
     toggleForm() {
         document.getElementById("house-form").classList.toggle("d-none")
     }
 }
+
+
+
